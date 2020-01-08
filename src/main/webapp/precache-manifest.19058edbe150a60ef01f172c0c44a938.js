e32f579a6bfedfa79cfcf8d955ccd96469ca8573
unknown <cph-md273@cphbusiness.dk>
2020-01-08 15:49:57 +0100

    eow

---
+++ src/main/webapp/precache-manifest.19058edbe150a60ef01f172c0c44a938.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e905f3fe1133956e4ebb9c4e2b10eaad",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b5e9e9c18845b660731b",
+    "url": "./static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "45aa3399703bfbeddf56",
+    "url": "./static/css/main.5e3284cb.chunk.css"
+  },
+  {
+    "revision": "b5e9e9c18845b660731b",
+    "url": "./static/js/2.ad4cb87a.chunk.js"
+  },
+  {
+    "revision": "45aa3399703bfbeddf56",
+    "url": "./static/js/main.7e5891d6.chunk.js"
+  },
+  {
+    "revision": "41937316effba11328d4",
+    "url": "./static/js/runtime-main.f6c32322.js"
+  }
+]);
